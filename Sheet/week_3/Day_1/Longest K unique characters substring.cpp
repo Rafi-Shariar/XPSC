91e461fb0cf017078bf6150ac24aae01a00f8796
Rafi <rafi.shariar619@gmail.com>
2023-06-16 12:50:35 +0600

    week 03 - Day 01 updated

--- Sheet/week_3/Day_1/Longest K unique characters substring.cpp
+++ Sheet/week_3/Day_1/Longest K unique characters substring.cpp
@@ -1,3 +1,5 @@
+//https://practice.geeksforgeeks.org/problems/longest-k-unique-characters-substring0853/1
+
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long int
--- Sheet/week_3/Day_1/Longest_Sub-Array_with_Sum K.cpp
+++ Sheet/week_3/Day_1/Longest_Sub-Array_with_Sum K.cpp
@@ -1,3 +1,5 @@
+//https://practice.geeksforgeeks.org/problems/longest-sub-array-with-sum-k0809/1
+
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long int
